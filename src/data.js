8f2812d3304cb7b8df53518e160e48f41405600e
kirillpotekhin <kirill.potekhin@gmail.com>
2019-10-07 00:04:47 +0700

    Изменил моковые данные начала и конца точки маршрута, чтобы было удобно отладить сортировку и филтрацию

--- src/data.js
+++ src/data.js
@@ -30,9 +30,9 @@ export const getRoute = () => ({
     `http://picsum.photos/300/150?r=${Math.random()}`,
   ],
   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `, Math.floor(1 + Math.random() * 3)),
-  getEventStartTime: Date.now() - 24 * 60 * 60 * 1000 * 2 + 24 * 60 * Math.floor(Math.random() * 160) * 1000,
+  getEventStartTime: Date.now() - 24 * 60 * 60 * 1000 * 2 + 24 * 60 * Math.floor(Math.random() * 260) * 1000,
   get getEventEndTime() {
-    return this.getEventStartTime + 24 * 60 * Math.floor(Math.random() * 60) * 1000;
+    return this.getEventStartTime + 24 * Math.floor(Math.random() * 40) * 60 * 1000;
   },
   price: Math.floor(Math.random() * 300),
   optionAll: [
