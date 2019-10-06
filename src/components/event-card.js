aa796519b68128eb6a86af89f95ff31d6a68c9a6
kirillpotekhin <kirill.potekhin@gmail.com>
2019-10-07 00:06:23 +0700

    Удалил обертку в шаблоне

--- src/components/event-card.js
+++ src/components/event-card.js
@@ -39,39 +39,37 @@ export default class Event extends AbstractComponent {
   }
 
   getTemplate() {
-    return `<li class="trip-events__item">
-      <div class="event">
-        <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${this._type}.png" alt="Event type icon">
-        </div>
-        <h3 class="event__title">${this._type} ${(this._type === ArrivalPoint.CHECK_IN || this._type === ArrivalPoint.RESTAURANT || this._type === ArrivalPoint.SIGHTSEEING) ? `in` : `to`} ${this._city}</h3>
-
-        <div class="event__schedule">
-          <p class="event__time">
-            <time class="event__start-time" datetime="${this.getTimeString(this._startTime)}">${this.getTime(this._startTime)}</time>
-            &mdash;
-            <time class="event__end-time" datetime="${this.getTimeString(this._endTime)}">${this.getTime(this._endTime)}</time>
-          </p>
-          <p class="event__duration">${this.getDifferenceTime()}</p>
-        </div>
-
-        <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">${this._price}</span>
-        </p>
-
-        <h4 class="visually-hidden">Offers:</h4>
-        <ul class="event__selected-offers">
-          ${Array.from(this._optionAll).filter((it) => it.isOption).slice(0, 2).map((option) => `<li class="event__offer">
-            <span class="event__offer-title">${option.title}</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">${option.price}</span>
-          </li>`).join(``) }
-        </ul>
-
-        <button class="event__rollup-btn" type="button">
-          <span class="visually-hidden">Open event</span>
-        </button>
+    return `<div class="event">
+      <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${this._type}.png" alt="Event type icon">
       </div>
-    </li>`;
+      <h3 class="event__title">${this._type} ${(this._type === ArrivalPoint.CHECK_IN || this._type === ArrivalPoint.RESTAURANT || this._type === ArrivalPoint.SIGHTSEEING) ? `in` : `to`} ${this._city}</h3>
+
+      <div class="event__schedule">
+        <p class="event__time">
+          <time class="event__start-time" datetime="${this.getTimeString(this._startTime)}">${this.getTime(this._startTime)}</time>
+          &mdash;
+          <time class="event__end-time" datetime="${this.getTimeString(this._endTime)}">${this.getTime(this._endTime)}</time>
+        </p>
+        <p class="event__duration">${this.getDifferenceTime()}</p>
+      </div>
+
+      <p class="event__price">
+        &euro;&nbsp;<span class="event__price-value">${this._price}</span>
+      </p>
+
+      <h4 class="visually-hidden">Offers:</h4>
+      <ul class="event__selected-offers">
+        ${Array.from(this._optionAll).filter((it) => it.isOption).slice(0, 2).map((option) => `<li class="event__offer">
+          <span class="event__offer-title">${option.title}</span>
+          &plus;
+          &euro;&nbsp;<span class="event__offer-price">${option.price}</span>
+        </li>`).join(``) }
+      </ul>
+
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+  </div>`;
   }
 }
